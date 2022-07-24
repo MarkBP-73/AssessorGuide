d722e9a4c86f668d510c86e3286521857249df3f
Mark Phillips <phillipsnb@cardiff.ac.uk>
2022-07-24 10:48:07 +0100

    Initial Commit

---
+++ AssessorGuide/AssessorGuideApp.swift
@@ -0,0 +1,17 @@
+//
+//  AssessorGuideApp.swift
+//  AssessorGuide
+//
+//  Created by Mark Phillips on 24/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct AssessorGuideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
